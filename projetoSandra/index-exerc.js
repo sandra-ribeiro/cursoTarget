05ef10eb7f84d5a7dac0dbe2e55daa1776051e8b
Sandra Maria <sandramaria@targettrust.local>
2018-11-20 11:36:47 -0200

    dddd

--- projetoSandra/index-exerc.js
+++ projetoSandra/index-exerc.js
@@ -8,3 +8,20 @@ function carregaImagens() {
         document.getElementById(ele).style.height = "100px";
     }
 }
+function Inserir(objeto) {
+    var imagens = new Array("img1","img2","img3","img4","img5","img6");
+    for(key in imagens){
+        var numero = parseInt(key) + parseInt(1);
+        var ele = "img" + numero;
+        var objNovoParagrafo = document.createElement('p');
+        var objNovaImagem = document.getElementById(ele).src = "images/"+"malvadofavor.jpg";
+        objNovoParagrafo.onclick = function () { this.parentNode.removeChild(this); };
+    }
+}
+
+function Insere(objeto) {
+    var imagens = new Array("img1","img2","img3","img4","img5","img6");
+    var objNovoParagrafo = document.createElement('p');
+    var objNovaImagem = document.getElementById("img1").src = "images/"+"img5.jpg";
+    objNovoParagrafo.onclick = function () { this.parentNode.removeChild(this); };
+}
